bd6917b96baef3023377ffb6e63515ca51561937
Florido Paganelli <florido.paganelli@hep.lu.se>
2025-10-09 13:48:37 +0200

    Helper files to create/ship the certficate bundle using tar

--- maintenance/tools/CAtools/README.md
+++ maintenance/tools/CAtools/README.md
@@ -1,1 +1,3 @@
 Suite of tools to manage CA and certificates
+
+The CA is not shared with this bundle for obvious security reasons.
---
+++ maintenance/tools/CAtools/setenv.sh
@@ -0,0 +1,6 @@
+# This script can be used to initialize the user environment for testing.
+basedir=$(dirname `readlink -f -- ${BASH_SOURCE:-$_}`)
+export X509_USER_CERT="$basedir/usercert.pem"
+export X509_USER_KEY="$basedir/userkey.pem"
+export X509_USER_PROXY="$basedir/userproxy.pem"
+export X509_CERT_DIR="$basedir/certificates"
